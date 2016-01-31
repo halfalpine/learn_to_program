2fd8e4558c9992b010a0bd15900dcb65d35cc091
halfalpine <rocksandwoks@gmail.com>
2016-01-31 18:30:12 -0500

    Begin 'Shuffle' method

---
+++ shuffle.rb
@@ -0,0 +1,14 @@
+=begin Y
+our goal: return a perfectly shuffled array.
+
+Question: How can you make sure you are getting a perfect shuffle?
+	use the random number generator
+Question: What would you even say a pefect shuffle would be?
+	generate a random number for each iteration of the method
+Question: Now test for it
+	redundant: trust the language to generate a true random number
+=end
+
+def shuffle some_array
+
+end
